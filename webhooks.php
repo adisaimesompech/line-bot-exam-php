dbf048a633ca0f6a203beebb5195973381ad2e76
adisaimesompech <45192411+adisaimesompech@users.noreply.github.com>
2018-11-20 14:07:29 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'jhteKXxCZOXz9jDX5SVxmrYiWEHA7qhlE+2x8kCZQiFirgbixhk87wYySaQpqK1HrJJygjuBqi2AooBQ/bGw8efgiMKHJiqQq4mCX9MaZXJWlZaXfpOspBeViumQDaFmloWKqIA8jbtFVTzzggXjkgdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
